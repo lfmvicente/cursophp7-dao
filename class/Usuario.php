4b6abf61feaae669f2950032490f8c6772fa1684
Luís Felipe <lfm.vicente@gmail.com>
2020-02-12 21:06:55 -0300

    Criamos Métodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -44,8 +44,6 @@ class Usuario{
 		$sql = new Sql();
 		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
 			":ID"=>$id
-
-
 		));
 
 		if(count($results) > 0){
@@ -56,6 +54,46 @@ class Usuario{
 			$this->setDeslogin($row['deslogin']);
 			$this->setDessenha($row['dessenha']);
 			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+		}
+
+	}
+
+	public static function getList(){
+
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+	}
+
+	public static function search($login){
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE ? ORDER BY deslogin", array(
+			':SEARCH'=>"%".$login."%"
+
+		));
+	}
+
+	public function login($login, $password){
+
+		$sql = new Sql();
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		));
+
+		if(count($results) > 0){
+
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+		}else{
+
+			throw new Exception("Login e/ou senha inválidos");
 
 		}
 
--- index.php
+++ index.php
@@ -2,11 +2,24 @@
 
 require_once ("config.php");
 
-$root = new Usuario();
+//carrega 1 usuario
+//$root = new Usuario();
+//$root->loadById(2);
+//echo $root;
 
-$root->loadById(2);
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("user","12345");
+
+echo $usuario;
 
 
 ?>
